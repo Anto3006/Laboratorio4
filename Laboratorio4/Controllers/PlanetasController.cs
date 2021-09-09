fc0bdea8f5f6dbd8729b275dea681410ad78d81c
Antonio Piedra <anto.piedrapach@hotmail.com>
2021-09-08 20:28:32 -0600

    Laboratorio 4.2 parte obligatoria terminada

--- Laboratorio4/Controllers/PlanetasController.cs
+++ Laboratorio4/Controllers/PlanetasController.cs
@@ -11,12 +11,79 @@ namespace Laboratorio4.Controllers
     public class PlanetasController : Controller
     {
         public ActionResult listadoDePlanetas()
-        { 
+        {
             PlanetasHandler accesoDatos = new PlanetasHandler();
             ViewBag.planetas = accesoDatos.obtenerTodoslosPlanetas();
             return View();
         }
 
+        public ActionResult crearPlaneta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult crearPlaneta(PlanetaModel planeta)
+        {
+            ViewBag.ExitoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    PlanetasHandler accesoDatos = new PlanetasHandler();
+                    ViewBag.ExitoAlCrear = accesoDatos.crearPlaneta(planeta);
+                    if (ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "El planeta" + " " + planeta.nombre + " fue creado con éxito :)";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Algo salió mal y no fue posible crear el planeta :(";
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult editarPlaneta(int? identificador)
+        {
+            ActionResult vista;
+            try
+            {
+                PlanetasHandler accesoDatos = new PlanetasHandler();
+                PlanetaModel planetaModificar = accesoDatos.obtenerTodoslosPlanetas().Find(smodel => smodel.id == identificador);
+                if (planetaModificar == null)
+                {
+                    vista = RedirectToAction("listadoDePlanetas");
+                }
+                else
+                {
+                    vista = View(planetaModificar);
+                }
+            }
+            catch
+            {
+                vista = RedirectToAction("listadoDePlanetas");
+            }
+            return vista;
+        }
+
+        [HttpPost]
+        public ActionResult editarPlaneta(PlanetaModel planeta)
+        {
+            try
+            {
+                PlanetasHandler accesoDatos = new PlanetasHandler();
+                accesoDatos.modificarPlaneta(planeta);
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
 
     }
 }
